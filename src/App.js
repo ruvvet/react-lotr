236206c406b1f0d2df2fc3b942846d6ee9cb4ade
Jenny <finej01@gmail.com>
2020-12-08 22:38:53 -0500

    react notes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,114 @@
 import logo from './logo.svg';
 import './App.css';
-import Movie from './Movie';
-
-const Movies = [
-  { title: 'The Fellowship of the Ring', hours: 2, min: 58 },
-  { title: 'The Two Towers', hours: 2, min: 59 },
-  { title: 'The Return of the King', hours: 3, min: 21 },
-];
+import Movie, { rating } from './Movie';
+import {useState} from 'react';
 
 
-const displayMovie = Movies.map((movie, i)=>{
-  return <Movie title={`The Lord of the Rings: ${movie.title}`}  hours={movie.hours} min={movie.min} key={i}/>
-})
+// 2 primary sets of data
+// component has 2 things: state + props
+// state is changeable, but props is not
+// parent can have a state that is passed to the child as a prop
+// child cannot change the prop (that used to be a state in the parent)
 
 function App() {
+
+
+  // what if its an array...
+  // use map/filter/reduce...
+  // since you can -RETURN- components from them
+  // const movies2 = [
+  //   { title: 'The Fellowship of the Ring', hours: 2, min: 58 },
+  //   { title: 'The Two Towers', hours: 2, min: 59 },
+  //   { title: 'The Return of the King', hours: 3, min: 21 },
+  // ];
+
+  // const displayMovie = movies2.map((movie, i)=>{
+  //   return <Movie title={`The Lord of the Rings: ${movie.title}`}  hours={movie.hours} min={movie.min} key={i}/>
+  // })
+
+
+
+
+  // what if its a nested object...
+  // use Object.entries/keys/values
+  // to turn it into an array
+
+  // const movies = {
+  //   1: { title: 'The Fellowship of the Ring', hours: 2, min: 58 },
+  //   2: { title: 'The Two Towers', hours: 2, min: 59 },
+  //   3: { title: 'The Return of the King', hours: 3, min: 21 },
+  // };
+
+
+
+  // changing the state....
+  // you can change the state in the parent
+  //but once state is passed into child as a prop
+  //it can no longer be changed in the child
+  const [movies, setMovies] = useState([
+    { title: 'The Fellowship of the Ring', hours: 2, min: 58 },
+    { title: 'The Two Towers', hours: 2, min: 59 },
+    { title: 'The Return of the King', hours: 3, min: 21 },
+  ]);
+
+  const handleClick = () =>{
+   setMovies([movies[1], movies[2], movies[0]])
+  }
+
+
+
+  const renderMovies = () => {
+    // can have an array of components
+    // and put them inside the return of another component
+    return movies.map((movie, i) => (
+      <Movie
+        title={`The Lord of the Rings: ${movie.title}`}
+        hours={movie.hours}
+        min={movie.min}
+        key={i}
+      />
+    ));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {/* <Movie /> */}
+      <button onClick={handleClick}>Shuffle</button>
 
+      {/* <Movie /> */}
 
-        {displayMovie}
-      </header>
+      {/* returning an array of mapped componenets */}
+      {/* {Object.values(movies).map((movie, i) => (
+          <Movie
+            title={`The Lord of the Rings: ${movie.title}`}
+            hours={movie.hours}
+            min={movie.min}
+            key={i}
+          />
+        ))} */}
+
+      {/* this is the map thing we did above  - can return an array */}
+      {/* {[
+          <Movie
+            title={`The Lord of the Rings: ${movies2[0].title}`}
+            hours={movies2[0].hours}
+            min={movies2[0].min}
+            key={0}
+          />,
+          <Movie
+            title={`The Lord of the Rings: ${movies2[1].title}`}
+            hours={movies2[1].hours}
+            min={movies2[1].min}
+            key={1}
+          />,
+          <Movie
+            title={`The Lord of the Rings: ${movies2[2].title}`}
+            hours={movies2[2].hours}
+            min={movies2[2].min}
+            key={2}
+          />,
+        ]} */}
+
+      {renderMovies()}
     </div>
   );
 }
--- src/Movie.js
+++ src/Movie.js
@@ -1,6 +1,15 @@
+export function rating() {
+  //calculate rating
+
+}
+
 function Movie(props) {
+
+  // props are immutable
+  // don't assign values to props
+  // don't change value of prop in the child
+  
   return (
-    // how to loop through this?
     <div>
       <h1> {props.title}</h1>
       <p>
@@ -11,3 +20,7 @@ function Movie(props) {
 }
 
 export default Movie;
+
+// exporting function movie
+// default export only exports what the thing is
+// any non-default export is an obj-key export
